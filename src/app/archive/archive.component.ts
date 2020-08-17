4b37b7f49560bcf8d653342fd91213eb919e8eea
malinez <33690546+malinez@users.noreply.github.com>
2020-08-17 15:12:02 +0300

    get selected translation in main component

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../service/data.service";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-archive',
@@ -9,7 +11,7 @@ export class ArchiveComponent implements OnInit {
 
   translations: Array<any>;
 
-  constructor() {
+  constructor(private router: Router, private data: DataService) {
     this.translations = [];
    }
 
@@ -18,5 +20,11 @@ export class ArchiveComponent implements OnInit {
 		localStorage.getItem('user-translation') ? this.translations = JSON.parse(localStorage.getItem('user-translation')) : this.translations = [];
 		console.log(this.translations);
   }
+
+  public openDocument(doc:object) {
+		console.log('Current document:', doc);
+		this.data.setTranslation(doc);
+		this.router.navigate(['/']);
+	}
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from "../service/data.service";
 
 @Component({
   selector: 'app-main',
@@ -12,7 +14,8 @@ export class MainComponent implements OnInit {
 	languages: Array<any>;
   translation: any;
   
-  constructor() {
+  constructor(private http: HttpClient, private data: DataService) {
+    
     this.translations = [];
 		this.languages = [
 			{ name: 'Русский', value: 'ru' },
@@ -31,18 +34,25 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('Created main component...');
-		localStorage.getItem('user-translation') ? this.translations = JSON.parse(localStorage.getItem('user-translation')) : this.translations = [];
+    localStorage.getItem('user-translation') ? this.translations = JSON.parse(localStorage.getItem('user-translation')) : this.translations = [];
+    
+    // get selected translation from archive
+    this.data.currentTranslation.subscribe(item => this.translation = item);
+    console.log('Current selected translation: ',this.translation);
+
+    // hide upload button to avoid uploading same translation
+    (Object.keys(this.translation).length > 0) ? this.showUploadBtn = false : this.showUploadBtn = true;
   }
 
   public setTranslation(item) {
-		console.log('Get: ', item);
-		this.translations.push(item);
-		localStorage.setItem('user-translation', JSON.stringify(this.translations));
-		this.clearForm();
-	}
+    console.log('Get: ', item);
+    this.translations.push(item);
+    localStorage.setItem('user-translation', JSON.stringify(this.translations));
+    this.clearForm();
+  }
 
   public clearForm() {
-		this.translation = {};
-	}
+	  this.translation = {};
+  }
 
 }
